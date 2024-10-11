5b5b7cd12460572a90e6c0b4bf5da345506be876
HakureiReimu18 <140368777+HakureiReimu18@users.noreply.github.com>
2024-10-11 15:53:41 +0800

    变更

---
+++ Lua/Autorun/init.lua
@@ -0,0 +1,15 @@
+TLE = {}
+TLE.Name="Touhou_Lua_Expansion"
+TLE.Version = "1.0"
+TLE.VersionNum = 01000000
+TLE.Path = table.pack(...)[1]
+
+dofile(TLE.Path.."/Lua/Scripts/Sever/Alice_Doll_Control_Change.lua")
+
+if CLIENT then
+	Timer.Wait(function()
+		local runstring = "\n/// Touhou Lua Expansion"..TLE.Version.." ///\n"
+		print(runstring)  
+	end,1)
+	dofile(TLE.Path.."/Lua/Scripts/Client/Touhou_Cam_Offset.lua")
+end
