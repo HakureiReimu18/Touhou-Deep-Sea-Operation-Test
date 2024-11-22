6f03b225c24211712bd32d1bec019ab4eefd5518
HakureiReimu18 <140368777+HakureiReimu18@users.noreply.github.com>
2024-11-22 20:07:16 +0800

    update

--- Lua/Scripts/Sever/Touhou_Hisoutensoku_Amor.lua
+++ Lua/Scripts/Sever/Touhou_Hisoutensoku_Amor.lua
@@ -1,37 +1,55 @@
---[[
-    This example adds a special type of armor that can absorb damage, and when it's condition reaches 0, it breaks.
---]]
 
-local function Clamp(n, min, max)
-    if n < min then
-        n = min
-    elseif n > max then
-        n = max
+TLE.Preapply = {}
+TLE.Postapply = {}
+
+local function pre_apply(characterHealth, attackResult,hitLimb)
+    for func in TLE.Preapply do
+        if func(characterHealth, attackResult,hitLimb) == true then
+            return true
+        end
     end
-
-    return n
+    return false
 end
 
-Hook.Add("character.applyDamage", "LuaArmor.ApplyDamage", function (characterHealth, attackResult, hitLimb)
+local function post_apply(characterHealth, attackResult,hitLimb)
+    for func in TLE.Postapply do
+        func(characterHealth, attackResult,hitLimb)
+    end
+end
+
+Hook.Add("character.applyDamage", "Touhou_Hisoutensoku_Amor.OnDamage", function (characterHealth, attackResult, hitLimb)
+
+    --precheck
+    if pre_apply(characterHealth, attackResult,hitLimb) then
+        return true
+    end
 
     local character = characterHealth.Character
-    if character.Inventory == nil then return end
+    if character.Inventory == nil or characterHealth == nil or characterHealth.Character.IsDead or hitLimb == nil or  attackResult == nil or attackResult.Afflictions == nil then
+        post_apply(characterHealth, attackResult, hitLimb)
+        return
+    end
+
     local armor = character.Inventory.GetItemInLimbSlot(InvSlotType.OuterClothes)
+
+
 
     if armor == nil or armor.Prefab.Identifier ~= "Touhou_Hisoutensoku" then return end
 
     local damage = 0
-    local CharacterResistance  = Clamp(characterHealth.GetResistance(affliction.Prefab) , 0, 0.99)
 
     for affliction in attackResult.Afflictions do
         damage = damage + affliction.Strength
     end
-
-    armor.Condition = armor.Condition - damage * (1 - CharacterResistance)
+    armor.Condition = armor.Condition - damage * 0.1
 
     if armor.Condition > 0 then
         return true
     else
+        Entity.Spawner.AddEntityToRemoveQueue(armor)
+    end
+    if armor.Condition <= 0 then
+        armor.Condition = 0 -- 确保 Condition 不为负值
         Entity.Spawner.AddEntityToRemoveQueue(armor)
     end
 end)
